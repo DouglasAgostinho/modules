225e22a8e0a9e9bb4427de2b2f0074552c85407f
DouglasAgostinho <marques.aut@gmail.com>
2024-06-01 21:38:10 -0300

    corrigido status pub/priv da struct Vessel & implementado teste

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 
-mod modules;
-use std::io;
+pub mod modules;
+//use std::io;
 
+use modules::components::Vessel;
 //use crate::ves
 
 fn main() {
     println!("Hello, world!");
 
-    let mut s: String = String::new();
+    
+    let m_01 = Vessel{
+        shape:          "cilinder",
+        range:          [0.0, 2000.0],
+        h_hh:           [1600.0, 1800.0],
+        l_ll:           [400.0, 200.0],
+        in_valve:       0.0,
+        in_pressure:    0.0,
+        out_valve:      0.0,
+        level:          0.0,
+    };
 
-    match io::stdin().read_line(&mut s) {
-
-        Ok(_) => println!(""),
-        Err(e) => println!("Error {}", e),
-        
-    }
-
-    println!("line {}", s);
+    let x = m_01.in_flow();
     
 }
--- src/modules.rs
+++ src/modules.rs
@@ -1,24 +1,25 @@
 
+//#[warn(dead_code)]
 pub mod components{
     
-    struct Vessel <'a> {
-        shape:          &'a str,
-        range:          [f32; 2],
-        h_hh:           [f32; 2],
-        l_ll:           [f32; 2],
-        in_valve:       f32,
-        in_pressure:    f32,
-        out_valve:      f32,
-        level:      f32,
+    pub struct Vessel <'a> {
+        pub shape:          &'a str,
+        pub range:          [f32; 2],
+        pub h_hh:           [f32; 2],
+        pub l_ll:           [f32; 2],
+        pub in_valve:       f32,
+        pub in_pressure:    f32,
+        pub out_valve:      f32,
+        pub level:      f32,
     }
     impl <'a> Vessel <'a>{
 
-        fn in_flow(&self) -> f32{
+        pub fn in_flow(&self) -> f32{
 
             self.in_valve * self.in_pressure
         }
 
-        fn out_flow(&self) -> f32{
+        pub fn out_flow(&self) -> f32{
 
             self.out_valve * (self.level / 100.0)
         }

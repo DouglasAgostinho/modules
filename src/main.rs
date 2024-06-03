631fd5f09e90b63e41413cfeb642a5efdaa4e617
DouglasAgostinho <marques.aut@gmail.com>
2024-06-02 23:43:24 -0300

    Added PID struct and contol function

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,49 @@
 pub mod modules;
 //use std::io;
 
-use modules::components::Vessel;
+use modules::components::{Vessel, PID};
 //use crate::ves
 
 fn main() {
     println!("Hello, world!");
 
     
-    let m_01 = Vessel{
+    let mut m_01 = Vessel{
         shape:          "cilinder",
         range:          [0.0, 2000.0],
         h_hh:           [1600.0, 1800.0],
         l_ll:           [400.0, 200.0],
-        in_valve:       0.0,
-        in_pressure:    0.0,
+        in_valve:       10.0,
+        in_pressure:    10.0,
         out_valve:      0.0,
         level:          0.0,
     };
 
-    let x = m_01.in_flow();
+    let mut pid = PID{
+        mv: 0.0,
+        sp: 500.0,
+        kp: 0.002,
+        ki: 0.0001,
+        kd: 0.002,
+    };
+
+    let mut i = 0.0;
+    let mut prev_time = 0.0;
+    let mut integral = 0.0;    
+    let mut e_prev = 0.0;
+
+    loop{
+
+        i += 1.0;
+
+        (prev_time, integral, e_prev ) = pid.control(i, prev_time, integral, e_prev, m_01.level, true);
+
+        m_01.level = m_01.level + m_01.in_flow() - m_01.out_flow();
+
+        m_01.out_valve = pid.mv;
+
+
+        println!(" Level is {}, input flow is {}, output flow is {}, outuput valve is {}", m_01.level, m_01.in_flow(), m_01.out_flow(), m_01.out_valve);
+    }
     
 }
--- src/modules.rs
+++ src/modules.rs
@@ -23,7 +23,70 @@ pub mod components{
 
             self.out_valve * (self.level / 100.0)
         }
+    }
 
+
+    pub struct PID {
+        pub mv: f32,
+        pub sp: f32,
+        pub kp: f32,
+        pub ki: f32,
+        pub kd: f32,
+    }
+    impl PID {
+        
+
+        pub fn control(&mut self, i: f32, mut prev_time: f32, integral: f32, mut e_prev: f32, pv: f32, debug: bool) -> (f32, f32, f32) {
+            use std::{thread, time};
+
+            //PID control logic        
+            let time = i * 0.1;
+                        
+            //calculations start        
+            let e: f32 = self.sp - pv;        
+            
+            let p: f32 = self.kp * e;        
+            
+            let integral = integral + (self.ki * e * (time - prev_time));
+
+            
+            let d: f32 = if i < 2.0 {
+                0.0
+            }
+            else{
+                self.kd * (e - e_prev) / (time - prev_time)
+            };        
+            
+            self.mv = (p + integral + d) * -1.0;  
+
+            if debug == true{
+                println!("\n -----{}-----", i);
+                println!("e = {}", e);
+                println!("p = {}, kp= {}", p, self.kp);
+                println!("integral = {}, ki = {}, time = {}, prev_time = {}", integral, self.ki, time, prev_time);
+                println!("d = {}, kd = {}", d, self.kd);
+                println!("Out valve = {}", self.mv);            
+                println!("Level is {:.2} & Setpoint is {:.2}", pv, self.sp);    
+                println!("\n -----{}-----", i);
+                thread::sleep(time::Duration::from_millis(250));
+            }      
+            
+            e_prev = e;
+            prev_time = time;                
+
+            if self.mv > 99.9 {
+                self.mv = 100.0
+            }
+            else if self.mv < 0.01 {
+                self.mv = 0.0
+                
+            }
+
+            (prev_time, integral, e_prev)
+            
+
+        }
+        
     }
 
 }
